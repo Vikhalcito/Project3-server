3e45bc440fd188f60a1caff0a9a102afe4b7d184
Victor Vikhal <victor.vikhal@gmail.com>
2025-06-20 20:57:35 +0200

    añadidas rutas para ejercicios de update, delete y get/id

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -12,6 +12,18 @@ router.get("/exercises", (req, res, next) => {
     .catch((err) => res.json(err));
 })
 
+//GET /api/exercises/:exerciseId => Ruta para renderizar en exercise en especifico
+
+router.get("/exercises/:exerciseId", (req, res, next) => {
+
+    const {exerciseId} = req.params
+
+    Exercise.findById(exerciseId)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.json(err));
+})
+
+
 //POST /api/exercises => Ruta para crear/añadir exercises
 
 router.post("/exercises", (req, res, next) => {
@@ -25,6 +37,38 @@ router.post("/exercises", (req, res, next) => {
         console.error("Error al crear el exercise", err);
         res.status(500).json({error: "Failed to create exercise"});
     })
+})
+
+//PUT /api/exercises/:exerciseId => Editar un exercise en especifico
+
+router.put("/exercises/:exerciseId", (req, res, next) => {
+    
+    const {exerciseId} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(exerciseId)) {
+        res.status(400).json({message: "Specified id is not valid"})
+        return;
+    }
+
+    Exercise.findByIdAndUpdate(exerciseId, req.body, {new: true})
+    .then((updatedExercise) => res.json(updatedExercise))
+    .catch((err) => res.json(err));
+});
+
+//DELETE /api/exercises/:exerciseId => Elimina/borra un exercise en especifico.
+
+router.delete("/exercises/:exerciseId", (req, res, next) => {
+    
+    const {exerciseId} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(exerciseId)) {
+        res.status(400).json({message: "Specified id is not valid"})
+        return;
+    }
+
+    Exercise.findByIdAndDelete(exerciseId)
+    .then(() => res.json({ message: `Task with ${taskId} is removed successfully.` }))
+    .catch((err) => res.json(err))
 })
 
 module.exports = router;
